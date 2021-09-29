1622b079e456097dddd1416b80313b057f214c7e
ylvain <ylvain.dev@ethics-gradient.org>
2021-09-29 08:23:37 +0200

    little cleanup of image builder code

--- src/sysimages/SysImageBuilder.jl
+++ src/sysimages/SysImageBuilder.jl
@@ -73,6 +73,7 @@ function julia(;
     project::Union{String,Nothing} = nothing,
     source::Union{String,Nothing} = nothing,
     code::Union{String,Nothing} = nothing,
+    trace::Union{String,Nothing} = nothing,
     indir::Union{String,Nothing} = nothing,
     image::Union{ImageSpec,Nothing} = nothing,
     optlvl::Int = 2,
@@ -82,6 +83,10 @@ function julia(;
     nrmpath(s) = isnothing(s) ? nothing : realpath(expanduser(s))
     escape(s) = isnothing(s) ? nothing : escape_string(s)
     addarg(cmd::Cmd, arg::Cmd) = Cmd(Cmd(vcat(cmd.exec, arg.exec)), cmd.ignorestatus, cmd.flags, cmd.env, cmd.dir)
+
+    addproj(cmd::Cmd, ::Nothing) = cmd
+    addproj(cmd::Cmd, val::String) = addarg(cmd, `--project=$val`)
+
     addflag(cmd::Cmd, ::String, ::Nothing) = cmd
     addflag(cmd::Cmd, flag::String, val::String) = addarg(cmd, `$flag $val`)
     addflag(cmd::Cmd, flag::String, val::Int) = addarg(cmd, `$flag $val`)
@@ -102,12 +107,14 @@ function julia(;
     )
     
     cmd = julia_invoke_cmd()
-    cmd = addflag(cmd, "--project", escape(nrmpath(project)))
+    cmd = addproj(cmd, nrmpath(project))
     cmd = addflag(cmd, "--sysimage", escape(img))
+    cmd = addflag(cmd, "--trace-compile", escape(trace))
+
     cmd = addflag(cmd, "-O", optlvl)
     cmd = addflag(cmd, "-g", dbglvl)
     cmd = addflag(cmd, "-e", code)
-    cmd = addflag(cmd, "--", escape(nrmpath(source)))
+    cmd = addflag(cmd, "--", nrmpath(source))
     cmd = Cmd(cmd; env, dir)
 
     if !isnothing(code)
@@ -288,12 +295,38 @@ function build(s::ImageSpec)
         error("unknow image")
     end
 
-    julia(project=builder, image=s, code="""
+    julia(project=builder, image=s, indir=dirname(path(s)), code="""
         using PackageCompiler
         using DevEnvConfig.SysImageBuilder
         image = $(repr(s))
+        ENV["JULIA_DEBUG"]="all"
         SysImageBuilder.$call(PackageCompiler.create_sysimage, image)
     """)
 end
+
+# julia> build(ImageSpec("stdlib-min"))
+# Julia execution code
+#     using PackageCompiler
+#     using DevEnvConfig.SysImageBuilder
+#     image = ImageSpec("stdlib-min")
+#     SysImageBuilder.build_stdlib_min(PackageCompiler.create_sysimage, image)
+# Julia execution starts...
+# PackageCompiler invoked in environment:
+#            PWD: C:\Users\Yoann\.julia\devenvmgr\v1.6-x86_64-w64-mingw32-haswell\stdlib-min
+# ACTIVE_PROJECT: C:\Users\Yoann\.julia\devenvmgr\v1.6-x86_64-w64-mingw32-haswell\Builder
+#      LOAD_PATH: C:\Users\Yoann\.julia\devenvmgr\v1.6-x86_64-w64-mingw32-haswell\Builder\Project.toml
+#     DEPOT_PATH: C:\Users\Yoann\.julia\devenvmgr\depot
+#   TARGET IMAGE: C:\Users\Yoann\.julia\devenvmgr\v1.6-x86_64-w64-mingw32-haswell\stdlib-min\sys.jsi
+#  CURRENT IMAGE: C:\Users\Yoann\AppData\Local\Programs\Julia-1.6.1\lib\julia\sys.dll
+#    DEFAULT JSI: true
+# Building Base+Stdlib from scratch. No precompilation.
+#     Updating registry at `C:\Users\Yoann\.julia\devenvmgr\depot\registries\General`
+#     Updating git-repo `https://github.com/JuliaRegistries/General.git`
+#   No Changes to `C:\Users\Yoann\.julia\devenvmgr\v1.6-x86_64-w64-mingw32-haswell\stdlib-min\Build\Project.toml`
+#   No Changes to `C:\Users\Yoann\.julia\devenvmgr\v1.6-x86_64-w64-mingw32-haswell\stdlib-min\Build\Manifest.toml`
+# [ Info: PackageCompiler: creating base system image (incremental=false)...
+# [ Info: PackageCompiler: creating system image object file, this might take a while...
+# Build completed: 99.0 MB, 2021-09-07T14:12:01.292
+# Julia execution completed.
 
 end # module SysImageBuilder
